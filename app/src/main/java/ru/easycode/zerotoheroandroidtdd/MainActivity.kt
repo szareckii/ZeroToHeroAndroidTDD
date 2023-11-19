aa50d4aa0e59cfb1ac77d1df1b7b7951a29852e7
Stas Zareckii <zareckii@gmail.com>
2023-11-19 17:46:06 +0300

    task/008 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.titleTextView)
+        val btn = findViewById<Button>(R.id.hideButton)
+
+        btn.setOnClickListener {
+            tv.visibility = View.INVISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+            outState.putInt(KEY, tv.visibility)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        tv.visibility = savedInstanceState.getInt(KEY)
+    }
+
+    companion object {
+        private const val KEY = "isShowText"
     }
 }
